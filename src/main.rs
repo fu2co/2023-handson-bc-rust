febeb0422b91d76c0cfedf506d95d1801f65e2df
Jurrian Fahner <jurrian.fahner@gmail.com>
2023-05-24 18:34:14 +0200

    add start presentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+struct Process {
+    active: bool,
+    id: isize
 }
+
+fn main() {
+    let ap = Process {active: true, id: 100};
+} 
+
+// demo topics
+// =============================
+// [ ] println macro
+// [ ] trait Debug / Display
+// [ ] move variable -> shallow copy
+// [ ] trait Clone -> deep copy
+// [ ] trait Copy
+// [ ] implement lifetimes
