c041b08f1bef0f853791c9850c99889b33a8a6b4
Jurrian Fahner <jurrian.fahner@gmail.com>
2023-05-24 18:46:51 +0200

    implement debug / display

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
+use std::fmt::{Debug, Display};
+
 #[allow(dead_code)]
 struct Process {
     active: bool,
     id: isize
 }
 
+impl Debug for Process {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Process").field("active", &self.active).field("id", &self.id).finish()
+    }
+}
+
+impl Display for Process {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.active {
+            false => write!(f, "Process not active"),
+            true => write!(f, "id: {}", self.id),
+        }
+    }
+}
+
 fn main() {
-    let ap = Process {active: true, id: 100};
-    println!("My id: {:?}", ap.id);
+    let ap = Process {active: false, id: 100};
+    println!("My process: {:?}", ap);
+    println!("My process: {}", ap);
 } 
 
 // demo topics
 // =============================
 // [x] println macro
-// [ ] trait Debug / Display
+// [x] trait Debug / Display
 // [ ] move variable -> shallow copy
 // [ ] trait Clone -> deep copy
 // [ ] trait Copy
